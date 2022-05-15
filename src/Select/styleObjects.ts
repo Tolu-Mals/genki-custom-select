b2f080a9e15b4976bdac609cc480a3229585695b
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-05-15 18:26:47 +0100

    feat: add focus state styles to unstyled variant

--- src/Select/styleObjects.ts
+++ src/Select/styleObjects.ts
@@ -2,108 +2,118 @@
 
   const outlinedVariantStyle = {
     light: {
+      '&:hover': {
+        borderColor: "#C5C6C8",
+      },
+      "&:focus":{
+        border: "1px solid #2F80ED",
+        boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+      },
+      border: "1px solid #E2E8F0",
+      borderRadius: "6px",
+      color: "#565661",
+      transition: "all 0.3s",
+      outline: "none"
+  },
+    dark: {
         '&:hover': {
-          borderColor: "#C5C6C8",
+          borderColor: "#ABABAB",
         },
         "&:focus":{
           border: "1px solid #2F80ED",
           boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
         },
-        border: "1px solid #E2E8F0",
+        border: "1px solid #596375",
         borderRadius: "6px",
-        color: "#565661",
+        color: "#C7C7C7",
         transition: "all 0.3s",
         outline: "none"
-    },
-      dark: {
-          '&:hover': {
-            borderColor: "#ABABAB",
-          },
-          "&:focus":{
-            border: "1px solid #2F80ED",
-            boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
-          },
-          border: "1px solid #596375",
-          borderRadius: "6px",
-          color: "#C7C7C7",
-          transition: "all 0.3s",
-          outline: "none"
-      }
+    }
   };
 
   const filledVariantStyle = {
     light: {
-        "&:hover": {
-          backgroundColor: "#e2ebf7"
+      "&:hover": {
+        backgroundColor: "#e2ebf7"
+      },
+      "&:focus":{
+        border: "1px solid #2F80ED",
+        boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+      },
+      backgroundColor: "#F0F5FC",
+      border: "1px solid #F0F5FC",
+      borderRadius: "6px",
+      color: "#565661",
+      transition: "all 0.3s",
+      outline: "none"
+
+    },
+    dark: {
+        '&:hover': {
+          backgroundColor: "#8FA0BD"
         },
         "&:focus":{
           border: "1px solid #2F80ED",
           boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
         },
-        backgroundColor: "#F0F5FC",
-        border: "1px solid #F0F5FC",
+        backgroundColor: "#7A889F",
+        border: "1px solid #7A889F",
         borderRadius: "6px",
-        color: "#565661",
+        color: "#151A23",
         transition: "all 0.3s",
-        outline: "none"
-  
-      },
-      dark: {
-          '&:hover': {
-            backgroundColor: "#8FA0BD"
-          },
-          "&:focus":{
-            border: "1px solid #2F80ED",
-            boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
-          },
-          backgroundColor: "#7A889F",
-          border: "1px solid #7A889F",
-          borderRadius: "6px",
-          color: "#151A23",
-          transition: "all 0.3s",
-          outline: "none",
-      }
+        outline: "none",
+    }
   };
 
   const flushedVariantStyle = {
     light: {
-        '&:hover': {
-          borderColor: "#C5C6C8"
+      '&:hover': {
+        borderColor: "#C5C6C8"
+      },
+      '&:focus': {
+        borderColor: "#2F80ED",
+        boxShadow: "rgb(49 130 206) 0px 0.5px",
+      },
+      transition: "all 0.3s",
+      color: "#565661",
+      borderBottom: "1px solid #E2E8F0",
+      paddingLeft: 0,
+      paddingRight: 0,
+      outline: "none"
+    },
+    dark: {
+        "&:hover": {
+          borderColor: "#ABABAB"
         },
-        '&:focus': {
+        "&:focus": {
           borderColor: "#2F80ED",
           boxShadow: "rgb(49 130 206) 0px 0.5px",
         },
-        transition: "all 0.3s",
-        color: "#565661",
-        borderBottom: "1px solid #E2E8F0",
+        transition: "border 0.3s",
+        color: "#C7C7C7",
+        borderBottom: "1px solid #596375",
         paddingLeft: 0,
         paddingRight: 0,
         outline: "none"
       },
-      dark: {
-          "&:hover": {
-            borderColor: "#ABABAB"
-          },
-          "&:focus": {
-            borderColor: "#2F80ED",
-            boxShadow: "rgb(49 130 206) 0px 0.5px",
-          },
-          transition: "border 0.3s",
-          color: "#C7C7C7",
-          borderBottom: "1px solid #596375",
-          paddingLeft: 0,
-          paddingRight: 0,
-          outline: "none"
-        },
   };
 
-  const unstyledVariantStyle = {
+  const unstyledVariantStyle =  {
     light: {
+        "&:focus": {
+          border: "1px solid #2F80ED",
+          boxShadow: "rgb(49 130 206) 0px 0.5px",
+        },
         color: "#565661",
+        outline: "none"
     },
     dark: {
+        "&:focus": {
+          borderColor: "#2F80ED",
+          boxShadow: "rgb(49 130 206) 0px 0.5px",
+        },
         color: "#C7C7C7",
+        outline: "none"
     }
   };
 
