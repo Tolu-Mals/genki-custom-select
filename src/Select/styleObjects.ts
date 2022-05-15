66a158041994e2ba59af6e73a3e2aaf2faa98ebe
Boluwatife Adekola-ojo <ojknation@gmail.com>
2022-05-15 08:31:11 +0100

    update styles

--- src/Select/styleObjects.ts
+++ src/Select/styleObjects.ts
@@ -2,43 +2,100 @@
 
 const outlinedVariantStyle = {
     light: {
-      border: "1px solid #E2E8F0",
-      borderRadius: "6px",
-      color: "#565661",
-    },
-    dark: {
-        border: "1px solid #596375",
+        '&:hover': {
+          borderColor: "#C5C6C8",
+        },
+        "&:focus":{
+          border: "1px solid #2F80ED",
+          boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+        },
+        border: "1px solid #E2E8F0",
         borderRadius: "6px",
-        color: "#C7C7C7",
-    }
+        color: "#565661",
+        transition: "all 0.3s",
+        outline: "none"
+    },
+      dark: {
+          '&:hover': {
+            borderColor: "#ABABAB",
+          },
+          "&:focus":{
+            border: "1px solid #2F80ED",
+            boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+          },
+          border: "1px solid #596375",
+          borderRadius: "6px",
+          color: "#C7C7C7",
+          transition: "all 0.3s",
+          outline: "none"
+      }
   };
 
   const filledVariantStyle = {
     light: {
-      backgroundColor: "#E2E8F0",
-      borderRadius: "6px",
-      color: "#565661",
-    },
-    dark: {
-        backgroundColor: "#7A889F",
+        "&:hover": {
+          backgroundColor: "#e2ebf7"
+        },
+        "&:focus":{
+          border: "1px solid #2F80ED",
+          boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+        },
+        backgroundColor: "#F0F5FC",
+        border: "1px solid #F0F5FC",
         borderRadius: "6px",
-        color: "#151A23",
-    }
+        color: "#565661",
+        transition: "all 0.3s",
+        outline: "none"
+  
+      },
+      dark: {
+          '&:hover': {
+            backgroundColor: "#8FA0BD"
+          },
+          "&:focus":{
+            border: "1px solid #2F80ED",
+            boxShadow: "rgb(49 130 206) 0px 0px 0px 1px",
+          },
+          backgroundColor: "#7A889F",
+          border: "1px solid #7A889F",
+          borderRadius: "6px",
+          color: "#151A23",
+          transition: "all 0.3s",
+          outline: "none",
+      }
   };
 
   const flushedVariantStyle = {
     light: {
-      color: "#565661",
-      borderBottom: "1px solid #E2E8F0",
-      paddingLeft: 0,
-      paddingRight: 0,
-    },
-    dark: {
-        color: "#C7C7C7",
-        borderBottom: "1px solid #666674",
+        '&:hover': {
+          borderColor: "#C5C6C8"
+        },
+        '&:focus': {
+          borderColor: "#2F80ED",
+          boxShadow: "rgb(49 130 206) 0px 0.5px",
+        },
+        transition: "all 0.3s",
+        color: "#565661",
+        borderBottom: "1px solid #E2E8F0",
         paddingLeft: 0,
         paddingRight: 0,
+        outline: "none"
       },
+      dark: {
+          "&:hover": {
+            borderColor: "#ABABAB"
+          },
+          "&:focus": {
+            borderColor: "#2F80ED",
+            boxShadow: "rgb(49 130 206) 0px 0.5px",
+          },
+          transition: "border 0.3s",
+          color: "#C7C7C7",
+          borderBottom: "1px solid #596375",
+          paddingLeft: 0,
+          paddingRight: 0,
+          outline: "none"
+        },
   };
 
   const unstyledVariantStyle = {
@@ -111,3 +168,4 @@ const outlinedVariantStyle = {
   }
 
   export default styleObjects
+
