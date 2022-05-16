5417cb793801dc1f23817bed66605ecb2b1c84c1
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-05-16 18:04:10 +0100

    feat: add readOnly button copy text functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Container>
         <Heading mb={4}>Custom Select Component</Heading>
         <Button onClick={toggleColorMode} mb={4}>Toggle dark</Button>
-        <Select variant="outlined" size="sm" placeholder="yippee" customIcon={<PhoneIcon />}>
+        <Select variant="outlined" size="lg" placeholder="yippee" customIcon={<PhoneIcon />} readOnly>
           <Option value="1">option one</Option>
           <Option value="2">option two</Option>
           <Option value="3">option three</Option>
--- src/Select/selectButton.tsx
+++ src/Select/selectButton.tsx
@@ -1,16 +1,22 @@
 import { ButtonProps } from "../types";
 import styleObjects from "./styleObjects";
 
-const { Flex, Text, useColorMode, createIcon, IconButton } = require("@chakra-ui/react");
+const { 
+  Flex,
+  Text,
+  useColorMode,
+  createIcon,
+  IconButton, 
+  Popover,
+  PopoverTrigger,
+  Portal,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody
+ } = require("@chakra-ui/react");
 
 const { ChevronDownIcon } = require("@chakra-ui/icons");
-// import copyIcon from "./copyIcon.svg";
-
-// const CopyIcon = () => (
-//   <Icon boxSize={8}>
-//     <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x=".25" y=".25" width="23.5" height="23.5" rx="3.75" fill="#F5F4F4" stroke="#E2E8F0" stroke-width=".5"/><path fill="#F5F4F4" d="M5 5h14v14H5z"/><path d="M16.667 6.167h-5.834c-.643 0-1.166.523-1.166 1.166v2.334H7.333c-.643 0-1.166.523-1.166 1.166v5.834c0 .643.523 1.166 1.166 1.166h5.834c.643 0 1.166-.523 1.166-1.166v-2.334h2.334c.643 0 1.166-.523 1.166-1.166V7.333c0-.643-.523-1.166-1.166-1.166Zm-9.334 10.5v-5.834h5.834v5.834H7.334Zm9.334-3.5h-2.334v-2.334c0-.643-.523-1.166-1.166-1.166h-2.334V7.333h5.834v5.834Z" fill="#A6A6A6"/></svg>
-//   </Icon>
-// )
 
 const CopyIcon = createIcon({
   displayName: "CopyIcon",
@@ -141,9 +147,43 @@ const SelectButton = (props: ButtonProps) => {
     customIcon?: any;
   }
 
+  const copyText = () => {
+    if(selectedOption !== undefined) {
+      navigator.clipboard.writeText(String(selectedOption));
+
+    }
+  }
+
   const getButtonIcon = ( getButtonIconProps: getButtonIconProps) => {
     if(getButtonIconProps.isReadOnly){
-      return ( <IconButton aria-label='copy value' icon={<CopyIcon boxSize={iconSizeMap[size]} />} /> );
+      return selectedOption ? (
+        
+        <Popover width={8}>
+          <PopoverTrigger>
+              <IconButton
+              aria-label='copy value' 
+              variant="unstyled" 
+              size={iconSizeMap[size]}
+              onClick={copyText} 
+              icon={<CopyIcon boxSize={iconSizeMap[size]} />} 
+              /> 
+          </PopoverTrigger>
+        <Portal>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>
+              <Text>Copied!</Text>
+            </PopoverBody>
+          </PopoverContent>
+        </Portal>
+      </Popover>
+      ): ( <IconButton
+      aria-label='copy value' 
+      variant="unstyled" 
+      size={iconSizeMap[size]}
+      icon={<CopyIcon boxSize={iconSizeMap[size]} />} 
+      /> )
     }
 
     else if (getButtonIconProps.customIcon) return getButtonIconProps.customIcon;
@@ -158,6 +198,8 @@ const SelectButton = (props: ButtonProps) => {
   const buttonStyle = getButtonStyle({ isRequired, isReadOnly, isDisabled, isInvalid });
 
   const buttonIcon = getButtonIcon({ isReadOnly, customIcon });
+
+
 
   return (
     <Flex 
