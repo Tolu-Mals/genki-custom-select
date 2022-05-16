dcfa2765d94fb4695143910ff20b52ca40c3edf7
Boluwatife Adekola-ojo <ojknation@gmail.com>
2022-05-16 12:29:31 +0100

    hide dropdown on click

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -21,12 +21,12 @@ const Select = (props: SelectProps): JSX.Element => {
   const [selectedOption, setSelectedOption] = React.useState<string | number>();
   const [showListBox, toggleListBox] = React.useState(false);
   const { getButtonProps, getListBoxProps } = useSelect(props);
-  const componentRef = React.useRef<HTMLDivElement>(null)
+  const clickAwayRef = React.useRef<HTMLDivElement>(null)
 
   const handleSelectToggle = () => toggleListBox(!showListBox)
 
   const handleClickAway = (e : any) => {
-    if (componentRef.current && !componentRef.current.contains(e.target)) {
+    if (clickAwayRef.current && !clickAwayRef.current.contains(e.target)) {
         handleSelectToggle()
       }
   }
@@ -45,18 +45,19 @@ const Select = (props: SelectProps): JSX.Element => {
     setSelectedOption(item);
   };
 
-  const attachOnclicktoOption = (child: any) =>
+  const attachPropsToOption = (child: any) =>
     React.cloneElement(child, {
       onClick: () => {
         handleSelectItem(child.props.children);
+        handleSelectToggle()
       },
       selected: child.props.children === selectedOption,
     });
 
-  const options = React.Children.map(props.children, attachOnclicktoOption);
+  const options = React.Children.map(props.children, attachPropsToOption);
 
   return (
-    <Box ref={componentRef}>
+    <Box ref={clickAwayRef}>
       <SelectButton {...getButtonProps} onClick={handleSelectToggle} selectedOption={selectedOption} />
       {showListBox && <SelectListBox {...getListBoxProps} options={options} />}
     </Box>
