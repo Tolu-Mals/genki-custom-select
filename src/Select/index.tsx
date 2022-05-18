659f675d63063b5ba26f3b00ef145ff3f400a7b3
Boluwatife Adekola-ojo <ojknation@gmail.com>
2022-05-18 05:31:15 +0100

    clean up code base

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 import { Box, chakra, useColorMode } from "@chakra-ui/react";
 import SelectButton from "./selectButton";
 import SelectListBox from "./selectListBox";
--- src/stories/genki.stories.tsx
+++ src/stories/genki.stories.tsx
@@ -3,7 +3,7 @@ import { Container, Stack } from "@chakra-ui/layout";
 import { Select, Option } from "../Select";
 
 export default {
-  title: "Components / Forms / Select",
+  title: "Components / Genki / Select",
   decorators: [
     (story: Function) => (
       <Container maxWidth="400px" mt="40px">
@@ -21,21 +21,15 @@ export const Plain = () => (
   </Select>
 );
 
-export const States = () => (
-  <Stack>
-    <Select placeholder="Invalid" isInvalid>
-      <Option value="Option 1">Option 1</Option>
-      <Option value="Option 2">Option 2</Option>
-      <Option value="Option 3">Option 3</Option>
-    </Select>
 
-    <Select placeholder="Disabled" isDisabled>
-      <Option value="Option 1">Option 1</Option>
-      <Option value="Option 2">Option 2</Option>
-      <Option value="Option 3">Option 3</Option>
-    </Select>
+export const Sizes = () => (
+  <Stack spacing={4}>
+      <Select placeholder={`lg size`} size={"lg"} />
+      <Select placeholder={`md size`} size={"md"} />
+      <Select placeholder={`sm size`} size={"sm"} />
   </Stack>
-);
+)
+
 
 export const Variants = () => (
   <Stack>
@@ -65,13 +59,22 @@ export const Variants = () => (
   </Stack>
 );
 
-export const Sizes = () => (
-  <Stack spacing={4}>
-      <Select placeholder={`lg size`} size={"lg"} />
-      <Select placeholder={`md size`} size={"md"} />
-      <Select placeholder={`sm size`} size={"sm"} />
+export const States = () => (
+  <Stack>
+    <Select placeholder="Invalid" isInvalid>
+      <Option value="Option 1">Option 1</Option>
+      <Option value="Option 2">Option 2</Option>
+      <Option value="Option 3">Option 3</Option>
+    </Select>
+
+    <Select placeholder="Disabled" isDisabled>
+      <Option value="Option 1">Option 1</Option>
+      <Option value="Option 2">Option 2</Option>
+      <Option value="Option 3">Option 3</Option>
+    </Select>
   </Stack>
-)
+);
+
 
 export const Controlled = () => {
   const [value, setValue] = React.useState("")
