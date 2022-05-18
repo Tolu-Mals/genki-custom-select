4b33f1a865aaade9c04a6ccf1af2186dfb74325b
Boluwatife Adekola-Ojo <37719470+ojknation@users.noreply.github.com>
2022-05-18 16:46:15 +0100

    Test/keyboard navigation (#12)
    * write test for arrow up and arrow down
    
    * change tests location
    
    * fix storybook

---
+++ src/__tests__/keyboardnav.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Select, Option } from "../Select/index";
+import SelectListBox from "../Select/selectListBox";
+
+
+describe("keyboard navigation", () => {
+
+  it("Should move down the list if the ArrowDownKey is Pressed", async () => {
+    render(
+      <Select>
+        <Option value="1">one</Option>
+        <Option value="2">two</Option>
+        <Option value="6">six</Option>
+        <Option value="8">eight</Option>
+      </Select>
+    );
+    const optionElements = screen.queryAllByTestId("select-option");
+    fireEvent.focus(optionElements[0]);
+    fireEvent.keyDown(optionElements[0], { key: "ArrowDown", code: 40 });
+    expect(optionElements[1]).toHaveClass("current");
+  });
+
+  
+  it("Should move up the list if the ArrowUpKey is Pressed", async () => {
+    render(
+      <Select>
+        <Option value="1">one</Option>
+        <Option value="2">two</Option>
+        <Option value="6">six</Option>
+        <Option value="8">eight</Option>
+      </Select>
+    );
+    const optionElements = screen.queryAllByTestId("select-option");
+    fireEvent.focus(optionElements[2]);
+    fireEvent.keyDown(optionElements[2], { key: "ArrowUp", code: 38 });
+    expect(optionElements[1]).toHaveClass("current");
+  });
+});
